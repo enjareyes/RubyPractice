133a8c18a463d98152a21592d80838c5cdd3c639
Enja Reyes <enja.reyes@gmail.com>
2014-12-12 17:07:56 -1000

    add file

---
+++ chris-pine-exercises/profiling.rb
@@ -0,0 +1,36 @@
+=begin 
+
+Chris Pine 14.4
+
+Even better profiling. After you do your profiling, see the slow parts of your program, 
+and either make them faster or learn to love them as they are, you probably don’t want 
+to see all of that profiling anymore. But (I hope) you’re too lazy to go back and delete 
+it all...especially because you might want to use it again someday. Modify the profile 
+method so you can turn all profiling on and off by changing just one line of code. 
+Just one word!
+=end
+
+
+
+
+def benchmarking(block_description, &block)
+
+	turn_on = false
+
+	if turn_on
+		start_time = Time.new
+		block.call
+		duration = (Time.new - start_time)
+		puts "#{block_description} : #{duration} seconds"
+	else 
+		block.call
+	end
+
+
+end
+
+
+
+
+
+
